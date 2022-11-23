c107971eacf79d25aff253d47eb13d2e906f0370
Hamza Asif <98582615+Hamzi-codes@users.noreply.github.com>
2022-11-23 17:51:15 +0500

    updated file

--- components/Image.jsx
+++ components/Image.jsx
@@ -3,10 +3,8 @@ import { Context } from "../Context"
 
 export default function Image({ className, img }) {
   return (
-    <div>
-      <div className={`${className} imageContainer`}>
-        <img src={img.url} className="imageGrid" />
-      </div>
+    <div className={`${className} imageContainer`}>
+      <img src={img.url} className="imageGrid" />
     </div>
   )
 }
--- utils/getClass.js
+++ utils/getClass.js
@@ -3,5 +3,5 @@ export function getClass(i) {
     return "big"
   } else if (i % 6 === 0) {
     return "wide"
-  } else "tall"
+  }
 }
