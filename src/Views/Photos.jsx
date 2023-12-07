fdb61696f304ab9c18d51a588c2e72e68cc4f895
Hamza Asif <hmziiiraj@gmail.com>
2023-12-07 18:19:07 +0500

    feat: added spinner

--- src/Views/Photos.jsx
+++ src/Views/Photos.jsx
@@ -1,18 +1,39 @@
 import { React, useContext } from 'react'
+import { ThreeDots } from 'react-loader-spinner'
+
 import { Context } from '../../Context/Context'
 import { getClass } from '../../utils/getClass'
 import Image from '../components/Image'
 import Header from '../components/Header'
 
 export default function Photos() {
-  const { allPhotos } = useContext(Context)
+  const { allPhotos, isFetching } = useContext(Context)
 
-  const photos = allPhotos.map((img, i) => <Image key={img.id} img={img} className={getClass(i)} />)
+  const photos = allPhotos?.map((img, i) => (
+    <Image key={img.id} img={img} className={getClass(i)} />
+  ))
+
+  console.log(isFetching)
 
   return (
     <>
       <Header />
-      <div className="photos">{photos}</div>
+      {isFetching ? (
+        <div className="loadingSpinner">
+          <ThreeDots
+            height="80"
+            width="100"
+            radius="9"
+            color="#293264"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div className="photos">{photos}</div>
+      )}
     </>
   )
 }
